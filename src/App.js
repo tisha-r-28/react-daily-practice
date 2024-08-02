152e0bec8dc64dec46ba6e17c1453a33de2b0966
tisha-r-28 <tisha298.rejoice@gmail.com>
2024-08-02 10:49:53 +0530

    importing variables in different method

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React from "react";
-
-const bg = {
-    backgroundColor: "#AFE1AF",
-    width: "100%",
-    height: "100vh"
-}
-const card = {
-    width: "50%",
-    height: "250px",
-    padding: "50px",
-    backgroundColor: "white",
-    borderRadius: "15px"
-}
-
-let greeting = "Good Morning";
-
-const currentDate = new Date(2024, 8, 1, 24);
-const hours = currentDate.getHours();
-if (hours > 12 && hours <= 0) {
-    greeting = "Good Mornig";
-} else if (hours > 18 && hours >= 12) {
-    greeting = "Good Afternoon";
-} else if (hours >= 18 && hours < 24) {
-    greeting = "Good Evening";
-} else {
-    greeting = "Good Night";
-}
+import * as variables from './components/Practice';
 
 function App() {
-
+    const { course, lname } = variables
     return (
         <>
             {/* <Covid />
              */}
             <>
-                <section style={bg} className="d-flex justify-content-center">
-                    <div style={card}>
-                        <h1 style={{ color: "#FAA0A0", fontSize: "90px" }}>Hello <span>{greeting}</span></h1>
-                    </div>
-                </section>
+                <h1>{variables.default}</h1>
+                <h1>{lname}</h1>
+                <h1>{course}</h1>
             </>
         </>
     );
